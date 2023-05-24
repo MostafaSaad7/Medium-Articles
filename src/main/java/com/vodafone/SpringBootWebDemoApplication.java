56f5f870027dd959250d15310a98bbbe9e8e4404
HelalM <mostafahelal7699@gmail.com>
2023-05-24 16:33:48 +0300

    modify config

--- src/main/java/com/vodafone/SpringBootWebDemoApplication.java
+++ src/main/java/com/vodafone/SpringBootWebDemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootWebDemoApplication {
-//	/api
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebDemoApplication.class, args);
 	}
